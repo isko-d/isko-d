b779477409a25043ce10e45997ee6e71311fa85f
PeppermintSnow <168705921+PeppermintSnow@users.noreply.github.com>
2025-11-22 09:29:44 +0800

    init: springboot template

---
+++ src/main/java/com/isko_d/isko_d/IskoDApplication.java
@@ -0,0 +1,13 @@
+package com.isko_d.isko_d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IskoDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IskoDApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isko_d/isko_d/IskoDApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isko_d.isko_d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IskoDApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
